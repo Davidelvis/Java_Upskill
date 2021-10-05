c1e2a3c3f3e9cb1733e00718d5d341ccd841b094
Davidelvis <komboelvis08@gmail.com>
2021-10-05 17:49:57 +0300

    Changes

--- Program1.java
+++ Program1.java
@@ -2,5 +2,6 @@ public class Program1 {
     //main method begins execution of java application
      public static void name(String[] args) {
         System.out.println("Welcome to Java programming");
+        System.out.print("Hello");
     } //end method main
 } //end class welcome
