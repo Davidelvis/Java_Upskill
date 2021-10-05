8a92abd23c55b78e05c13df62b1460da8567ebb5
Davidelvis <komboelvis08@gmail.com>
2021-10-05 17:43:24 +0300

    Java class

---
+++ Program1.java
@@ -0,0 +1,6 @@
+public class Program1 {
+    //main method begins execution of java application
+     public static void name(String[] args) {
+        System.out.println("Welcome to Java programming");
+    } //end method main
+} //end class welcome
